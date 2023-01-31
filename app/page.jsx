66764b2a30ebe9d8a2b69b72c7d876a1082906ef
Support Axelcloud <support@axelcloud.com.my>
2023-01-31 15:30:39 +0800

    first init

---
+++ app/page.jsx
@@ -0,0 +1,25 @@
+import Movie from "./movie"
+
+export default async function Home() {
+  var data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+    )
+  const res = await data.json()  
+  console.log(res)
+  return (
+    <main>
+      <h1 className="">Hello Next 13 🔥</h1>
+      <div className="grid gap-16 grid-cols-fluid">
+      {res.results.map(movie => (
+          <Movie 
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            release_date={movie.release_date}
+          />
+      ))}
+      </div>
+    </main>
+  )
+}
